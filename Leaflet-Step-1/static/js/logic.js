9f619e381974a00f9b223ff91a5dd43179841947
borregoma <mborrego57@gmail.com>
2021-07-22 01:47:40 -0700

    updated index

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -22,7 +22,7 @@ function createMap(earthQuakes) {
 
 
     // Create Map 
-    var map = L.map("mapid", {
+    var myMap = L.map("mapid", {
         center: [39, -105],
         zoom: 2,
         layers: [lightmap,earthQuakes]
@@ -32,48 +32,48 @@ function createMap(earthQuakes) {
 
     L.control.layers(baseMaps, overlayMaps, {
        collapsed: false
-    }).addTo(map);
+    }).addTo(myMap);
 }
 
 function createMarkers(response) {
 
-      //  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
     // Pull the "locations" property off of response.data
-        var features = response.features;
+           var features = response.features;
 
      // Initialize an array to hold Earthquakes markers
 
-        var quakesArray = [];
+           var quakesArray = [];
 
     // Loop through the Earthquake locations array
     // For each location, create a marker and bind a popup with the location's name
 
-        for (var i=0; i < features.lenght; i++){
-            var feature = features [i];
-            var property = feature.properties.place;
-            var geometry = feature.geometry.coordinates;
-            var mag = feature.properties.mag;
-            var id = feature.properties.id;
-            var type = feature.geometry.type;
-            var quakeMarker = L.circleMarker([geometry[0], geometry[1]], {
-                fillOpacy: .5,
-                weight: .5,
-                color: "orange",
-                fillColor: quakeMarkerColor(geometry[2]),
-                radius: (mag * 5)
+           for (var i=0; i < features.lenght; i++){
+               var feature = features [i];
+               var property = feature.properties.place;
+               var geometry = feature.geometry.coordinates;
+               var mag = feature.properties.mag;
+               var id = feature.properties.id;
+               var type = feature.geometry.type;
+               var quakeMarker = L.circleMarker([geometry[0], geometry[1]], {
+                   fillOpacy: .5,
+                   weight: .5,
+                   color: "orange",
+                   fillColor: quakeMarkerColor(geometry[2]),
+                   radius: (mag * 5)
             }).bindPopup(`<h1>${id}</h1><hr><h3>Location: ${property}</h3><hr><h3>Magnitude: ${mag}</h3>`);
 
      // Add the marker to the quakesArray array
-            quakesArray.push(quakeMarker);
-        }
+                   quakesArray.push(quakeMarker);
+
+            }
 
   // Create a layer group made from the Earthquake markers array, pass it into the createMap function
-  createMap(L.layerGroup(quakesArray));     
-
+  createMap(L.layerGroup(quakesArray));
+       
 }
 
 
 // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
-  
+ 
